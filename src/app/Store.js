18fd69f43f82b4fa21eb2b96a5b5c7d7c00c597f
alimurtazaathar <alimurtazaathar@gmail.com>
2024-08-03 02:25:43 +0500

    more states and modified reducers

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,9 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import postReducers from "../features/posts/postSlice"
-
+import userReducers from "../features/users/usersSlice"
+import authReducers from "../features/auth/authSlice"
 export default configureStore({
     reducer:{
-        post:postReducers
+        post:postReducers,
+        users:userReducers,
+        auth:authReducers,
     }
 
 })
